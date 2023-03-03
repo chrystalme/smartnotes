2464ffa7d26e215810b71ec63662fb0ce30ff200
Afam Ifeanyi(Chrys) M <afam_ifeanyi@live.com>
2023-03-03 11:31:11 +0200

    Add bootstrap and made a base template

--- home/urls.py
+++ home/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.HomeView.as_view(), name='index'),
+    path('home', views.HomeView.as_view(), name='index'),
     path('authorized', views.AuthorizedView.as_view(), name="authorized")
 ]
